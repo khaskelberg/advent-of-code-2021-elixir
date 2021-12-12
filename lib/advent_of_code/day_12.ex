3fa208b5b8e809dd8658df0644c51a9d5b7a07a6
simon <simonkhaskelberg@gmail.com>
2021-12-12 19:44:41 +0300

    day 12

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -1,7 +1,54 @@
 defmodule AdventOfCode.Day12 do
   def part1(args) do
+    graph = parse_input(args)
+    traverse_graph(graph, &can_visit_p1?/2)
   end
 
   def part2(args) do
+    graph = parse_input(args)
+    traverse_graph(graph, &can_visit_p2?/2)
+  end
+
+  def traverse_graph(graph, can_visit?) do
+    traverse_graph(graph, "start", [], can_visit?)
+  end
+
+  def traverse_graph(_graph, "end", _walked, _can_visit?), do: 1
+
+  def traverse_graph(graph, edge, walked, can_visit?) do
+    walked = if downcase?(edge), do: [edge | walked], else: walked
+
+    graph
+    |> :digraph.out_neighbours(edge)
+    |> Enum.filter(&can_visit?.(&1, walked))
+    |> Enum.map(&traverse_graph(graph, &1, walked, can_visit?))
+    |> Enum.sum()
+  end
+
+  # Part 1. Allowed only to visit every small cave once
+  defp can_visit_p1?(edge, walked), do: edge not in walked
+
+  # Part 2. Allowed to visit single small cave twice
+  defp can_visit_p2?("start", _walked), do: false
+
+  defp can_visit_p2?(edge, walked) do
+    edge not in walked || length(Enum.uniq(walked)) == length(walked)
+  end
+
+  defp downcase?(str) do
+    String.downcase(str) == str
+  end
+
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, "-", trim: true))
+    |> Enum.reduce(:digraph.new(), fn [e1, e2], graph ->
+      :digraph.add_vertex(graph, e1)
+      :digraph.add_vertex(graph, e2)
+      :digraph.add_edge(graph, e1, e2)
+      :digraph.add_edge(graph, e2, e1)
+      graph
+    end)
   end
 end
--- test/advent_of_code/day_12_test.exs
+++ test/advent_of_code/day_12_test.exs
@@ -3,19 +3,29 @@ defmodule AdventOfCode.Day12Test do
 
   import AdventOfCode.Day12
 
+  @input """
+  start-A
+  start-b
+  A-c
+  A-b
+  b-d
+  A-end
+  b-end
+  """
+
   @tag :skip
   test "part1" do
-    input = nil
+    input = @input
     result = part1(input)
 
-    assert result
+    assert result == 10
   end
 
   @tag :skip
   test "part2" do
-    input = nil
+    input = @input
     result = part2(input)
 
-    assert result
+    assert result == 36
   end
 end
