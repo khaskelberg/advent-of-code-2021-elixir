14480957974e9f71ac7bed38d34e708aec6b6fe3
simon <simonkhaskelberg@gmail.com>
2021-12-15 16:23:50 +0300

    day 15

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,7 +1,77 @@
 defmodule AdventOfCode.Day15 do
   def part1(args) do
+    matrix = parse_input(args)
+    shortest_path(matrix)
+  end
+
+  def shortest_path(matrix) do
+    paths =
+      matrix
+      |> Enum.into(%{}, fn {coord, _} -> {coord, nil} end)
+      |> Map.put({0, 0}, 0)
+
+    q = :queue.new()
+    q = :queue.in({0, 0}, q)
+    paths = shortest_path(:queue.out(q), paths, matrix)
+    {_, shortest_path} = Enum.max_by(paths, fn {{x, y}, _} -> x * y end)
+    shortest_path
+  end
+
+  defp shortest_path({:empty, _}, paths, _matrix), do: paths
+
+  defp shortest_path({{:value, {x, y} = coord}, queue}, paths, matrix) do
+    neighboors = [{x - 1, y}, {x + 1, y}, {x, y - 1}, {x, y + 1}]
+    risk_current = Map.get(paths, coord)
+
+    {paths, queue} =
+      Enum.reduce(neighboors, {paths, queue}, fn neighboor, {paths, q} ->
+        cost = Map.get(matrix, neighboor)
+        risk_neighboor = Map.get(paths, neighboor)
+
+        case cost do
+          nil ->
+            {paths, q}
+
+          c when risk_current + c < risk_neighboor ->
+            {Map.put(paths, neighboor, risk_current + c), :queue.in(neighboor, q)}
+
+          _ ->
+            {paths, q}
+        end
+      end)
+
+    shortest_path(:queue.out(queue), paths, matrix)
   end
 
   def part2(args) do
+    matrix = parse_input(args)
+
+    size =
+      matrix
+      |> map_size()
+      |> :math.sqrt()
+      |> trunc()
+
+    matrix
+    |> Enum.reduce(%{}, fn {{x, y}, risk}, full_matrix ->
+      for i <- 0..4,
+          j <- 0..4,
+          into: full_matrix do
+        new_risk = risk + i + j
+        new_risk = if new_risk >= 10, do: rem(new_risk, 9), else: new_risk
+        {{size * i + x, size * j + y}, new_risk}
+      end
+    end)
+    |> shortest_path()
+  end
+
+  def parse_input(input) do
+    lines = String.split(input, "\n", trim: true)
+
+    for {line, x} <- Enum.with_index(lines),
+        {risk, y} <- Enum.with_index(String.to_charlist(line)),
+        into: %{} do
+      {{x, y}, risk - ?0}
+    end
   end
 end
--- test/advent_of_code/day_15_test.exs
+++ test/advent_of_code/day_15_test.exs
@@ -3,19 +3,32 @@ defmodule AdventOfCode.Day15Test do
 
   import AdventOfCode.Day15
 
+  @input """
+  1163751742
+  1381373672
+  2136511328
+  3694931569
+  7463417111
+  1319128137
+  1359912421
+  3125421639
+  1293138521
+  2311944581
+  """
+
   @tag :skip
   test "part1" do
-    input = nil
+    input = @input
     result = part1(input)
 
-    assert result
+    assert result == 40
   end
 
   @tag :skip
   test "part2" do
-    input = nil
+    input = @input
     result = part2(input)
 
-    assert result
+    assert result == 315
   end
 end
